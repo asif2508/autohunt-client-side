95c00058cc837beccab27e4dbac1b4751ba3601a
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-01 03:26:28 +0600

    about us and brands sections done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,18 +6,19 @@ import banner2 from '../../images/banner/banner2.jpg';
 import banner3 from '../../images/banner/banner3.jpg';
 import useInventory from '../../hooks/useInventory';
 import InventoryItem from '../InventoryItem/InventoryItem';
-import brand1 from '../../images/brands/Frame 85.png'
-import brand2 from '../../images/brands/Frame 86.png'
-import brand3 from '../../images/brands/Frame 87.png'
-import brand4 from '../../images/brands/Frame 88.png'
-import brand5 from '../../images/brands/Frame 89.png'
-import brand6 from '../../images/brands/Frame 90.png'
+import brand1 from '../../images/brands/Frame 85.png';
+import brand2 from '../../images/brands/Frame 86.png';
+import brand3 from '../../images/brands/Frame 87.png';
+import brand4 from '../../images/brands/Frame 88.png';
+import brand5 from '../../images/brands/Frame 89.png';
+import brand6 from '../../images/brands/Frame 90.png';
+import about from '../../images/about/Rectangle 110.png';
 const Home = () => {
     const [inventories] = useInventory();
     return (
         <div className='home-style'>
             <section clasName="banner-section">
-                <Container fluid>
+                <Container fluid className='p-0'>
                     <Carousel fade className='p-0'>
                         <Carousel.Item>
                             <img
@@ -62,7 +63,7 @@ const Home = () => {
                 <h3 className='mb-3'>Inventory Items</h3>
                 <div className='inventories'>
                     <Container>
-                        <Row className='g-5'>
+                        <Row className='gy-5'>
                             {
                                 inventories.map(inventory => <InventoryItem
                                     inventory={inventory}
@@ -104,10 +105,41 @@ const Home = () => {
                 <Container>
                     <Row className='g-3'>
                         <Col xs={12} md={6} lg={6}>
-
+                            <div className='text-start pe-5 '>
+                                <h3>About Us</h3>
+                                <p>We are the most well known car seller in the city.
+                                    We sell all the cars from best and well known brands.
+                                    We give so much discounts and we provide you the best
+                                    cashback policy. Our customer service is always open for you.
+                                </p>
+                            </div>
+                            <div>
+                                <Row className='g-5'>
+                                    <Col xs={6} md={6} lg={6}>
+                                        <h3>150</h3>
+                                        <hr />
+                                        <p>Vehicle In Stock</p>
+                                    </Col>
+                                    <Col xs={6} md={6} lg={6}>
+                                    <h3>40</h3>
+                                        <hr />
+                                        <p>Sold Car</p>
+                                    </Col>
+                                    <Col xs={6} md={6} lg={6}>
+                                    <h3>38</h3>
+                                        <hr />
+                                        <p>Happy Customer</p>
+                                    </Col>
+                                    <Col xs={6} md={6} lg={6}>
+                                    <h3>5</h3>
+                                        <hr />
+                                        <p>Awards</p>
+                                    </Col>
+                                </Row>
+                            </div>
                         </Col>
                         <Col xs={12} md={6} lg={6}>
-                            
+                            <Image src={about} fluid></Image>
                         </Col>
                     </Row>
                 </Container>
