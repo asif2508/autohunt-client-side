73f6df7f9bd9432b4fbb1ada06e3593b76a6fcab
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-01 01:11:57 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import NotFound from './components/NotFound/NotFound';
 import Register from './components/Register/Register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,38 @@
 import React from 'react';
-
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../../images/logo/Navigation/Navigation/car logo.png';
+import {Link} from 'react-router-dom';
+import './Header.css';
 const Header = () => {
     return (
         <div>
-            <h1>This is header</h1>
+            <Navbar bg="dark" variant='dark' expand="lg">
+                <Container>
+                    <Navbar.Brand href="#">
+                        <img
+                            src={logo}
+                            width="90"
+                            height="40"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="ms-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link as={Link} className='nav-style' to=''>Home</Nav.Link>
+                            <Nav.Link as={Link} className='nav-style' to='/blogs'>Blogs</Nav.Link>
+                            <Nav.Link as={Link} className='nav-style' to='/login'>Login</Nav.Link>
+                            <Nav.Link as={Link} className='nav-style' to='/register'>Register</Nav.Link>
+                            
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
