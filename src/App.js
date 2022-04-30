a5098764460a9f8d2a89b849948bc3d0374690c7
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-04-30 22:52:01 +0600

    primary routes added and router installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import NotFound from './components/NotFound/NotFound';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
