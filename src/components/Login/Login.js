562cf147da728379e0d6af1eeb102904bac85b9b
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-01 16:02:24 +0600

    login page design done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,41 @@
 import React from 'react';
-
+import { Button, Form } from 'react-bootstrap';
+import './Login.css';
+import { Link } from 'react-router-dom';
 const Login = () => {
     return (
-        <div>
-           <h1>This is Login</h1> 
+        <div className='login-style'>
+            <div className='login-form-style w-25 mx-auto'>
+                <h3 className='mb-2'>Login</h3>
+                <form>
+                    <Form.Floating className="mb-3">
+                        <Form.Control
+                            id="floatingInputCustom"
+                            type="email"
+                            placeholder="name@example.com"
+                            className='input-box'
+                        />
+                        <label htmlFor="floatingInputCustom">Email address</label>
+                    </Form.Floating>
+                    <Form.Floating>
+                        <Form.Control
+                            id="floatingPasswordCustom"
+                            type="password"
+                            placeholder="Password"
+                            className='input-box'
+                        />
+                        <label htmlFor="floatingPasswordCustom">Password</label>
+                    </Form.Floating>
+                    <div className='d-flex justify-content-between'>
+                        <Form.Group className="mt-2" id="formGridCheckbox">
+                            <Form.Check className='text-light' type="checkbox" label="remember me" />
+                        </Form.Group>
+                        <Button variant="link">reset password</Button>
+                    </div>
+                    <input className='submit-btn mt-1' type="submit" value="Login" />
+                </form>
+                <p className='text-start mt-2'>Don't have an account? <Link to='/register'>Register now</Link></p>
+            </div>
         </div>
     );
 };
