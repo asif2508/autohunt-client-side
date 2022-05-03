ce24de8e315283f59461f086e6b0d78b1faf90cf
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-04 04:32:53 +0600

    register implemented

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import { Link } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword, useSendEmailVerification, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
 import auth from '../firebase.init';
+import Loading from '../Loading/Loading';
 import Social from '../Social/Social';
 import './Register.css';
 const Register = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [message, setMessage] = useState('');
+    const [
+        createUserWithEmailAndPassword,
+        loading1,
+        error1,
+      ] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+    const [checkValue, setCheckValue] = useState(false);
+    const [sendEmailVerification, sending, error2] = useSendEmailVerification(auth);
 
+    if(loading1 || loading){
+        return <Loading></Loading>;
+    }
+    if(user){
+        navigate('/home');
+    }
     const handleSignInWithGoogle =() =>{
         signInWithGoogle();
+    }
+
+    const handleCreateUser = async event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirmPassword = event.target.confirmPassword.value;
+        if(password.length >= 6){
+            if(password === confirmPassword){
+                await createUserWithEmailAndPassword(email, password);
+                await sendEmailVerification();
+                
+            }else{
+                setMessage("Passwords didn't match!");
+            }
+        }else{
+            setMessage('Password length should be at least 6!');
+        }
+        if(user){
+            navigate('/home');
+        }
     }
     return (
         <div className='register-style'>
             <div className='login-form-style mx-auto'>
                 <h3 className='mb-3'>Create a New account</h3>
-                <form>
+                <form onSubmit={handleCreateUser}>
                 <Form.Floating className="mb-3">
                         <Form.Control
                             id="floatingInputCustom"
                             type="name"
                             placeholder="Your name"
                             className='input-box'
+                            name='name'
+                            required
                         />
                         <label htmlFor="floatingInputCustom">Your name</label>
                     </Form.Floating>
@@ -31,6 +71,8 @@ const Register = () => {
                             type="email"
                             placeholder="name@example.com"
                             className='input-box'
+                            name='email'
+                            required
                         />
                         <label htmlFor="floatingInputCustom">Email address</label>
                     </Form.Floating>
@@ -40,6 +82,8 @@ const Register = () => {
                             type="password"
                             placeholder="Password"
                             className='input-box'
+                            name='password'
+                            required
                         />
                         <label htmlFor="floatingPasswordCustom">Password</label>
                     </Form.Floating>
@@ -49,13 +93,17 @@ const Register = () => {
                             type="password"
                             placeholder="ConfirmPassword"
                             className='input-box'
+                            name='confirmPassword'
+                            required
                         />
                         <label htmlFor="floatingPasswordCustom">Confirm Password</label>
                     </Form.Floating>
                         <Form.Group className="mt-3 mb-2" id="formGridCheckbox">
-                            <Form.Check className='text-light text-start' type="checkbox" label="Accept terms and conditions" />
+                            <Form.Check onClick={()=> setCheckValue(!checkValue)} className='text-light text-start' type="checkbox" label="Accept terms and conditions" />
                         </Form.Group>
-                    <input className='submit-btn mt-1' type="submit" value="Register" />
+                        {(error || error1 || error2) && <p className='text-danger mb-0 text-start'>{error.message}</p>}
+                        {message && <p className='text-danger mb-0 text-start'>{message}</p>}
+                    <input className={!checkValue ? 'w-100 mt-2 register-disabled' : 'submit-btn mt-2'} type="submit" value="Register" disabled={!checkValue}/>
                 </form>
                 <p className='text-start mt-2'>Already have an account? <Link to='/login'>Login</Link></p>
                 <Social
