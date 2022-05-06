9539ee1403b91d1cc6b115362ea99dfeb84ae2ad
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-05-06 07:28:41 +0600

    my items page designed and added functions

---
+++ src/components/MyItem/MyItem.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Card, Button, Col, Row, Image } from 'react-bootstrap';
+import './MyItem.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
+const MyItem = ({ item }) => {
+    const [user, loading, error] = useAuthState(auth);
+    const { _id, name, brand, quantity, desc, price, sold, img } = item;
+    return (
+        <div className='mt-3'>
+            <Card className='inventory-card-style'>
+                <Card.Header as="h5">Item Added by you</Card.Header>
+                <Card.Body>
+                    <Row>
+                        <Col xs={12} md={6} lg={6}>
+                            <Image className='p-3' src={img} fluid></Image>
+                        </Col>
+                        <Col xs={12} md={6} lg={6}>
+                            <Card.Title className='text-start'>Name: {name}</Card.Title>
+                            <Card.Text className='text-start'>
+                                <p>Id: {_id}</p>
+                                <p>Added by: {user?.email}</p>
+                                <p>Supplier: {brand}</p>
+                                <p>Description: {desc}</p>
+                                <p>Price: {price}</p>
+                                <p>Quantity: {quantity}</p>
+                                <p>Sold: {sold}</p>
+
+                            </Card.Text>
+                        </Col>
+                    </Row>
+                    <Button className='myitem-delete' variant="primary">Delete</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default MyItem;
--- src/components/MyItems/MyItems.js
+++ src/components/MyItems/MyItems.js
@@ -1,9 +1,31 @@
-import React from 'react';
-
+import { async } from '@firebase/util';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import MyItem from '../MyItem/MyItem';
 const MyItems = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const [myItems, setMyitems] = useState([]);
+    useEffect(()=>{
+        const email = user?.email;
+        const url = `http://localhost:5000/myitems/${email}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setMyitems(data))
+    },[user])
     return (
-        <div>
-            <h1>This is my items</h1>
+        <div className=' myitem-page-style '>
+            <Container>
+                <h3 className='pt-2'>Items You have added</h3>
+            {
+                 myItems.map(item => <MyItem
+                 key={item.id}
+                 item={item}
+                 ></MyItem>)
+              
+            }
+            </Container>
         </div>
     );
 };
